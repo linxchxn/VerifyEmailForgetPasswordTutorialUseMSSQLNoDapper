4b0332a210bc82237bca798d987483ba3d50beb3
linxchxn <tho.dalin@outlook.com>
2022-12-21 10:12:10 +0700

    Add project files.

---
+++ VerifyEmailForgetPasswordTutorial/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace VerifyEmailForgetPasswordTutorial.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("SERVER=DESKTOP-3CN4L8E; Database=userdb;User ID=dalintho;Password=Dalin@321; Trusted_Connection = True; TrustServerCertificate = True;");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
